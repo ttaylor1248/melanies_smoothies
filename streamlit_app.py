113e6cd6aade2b0f3aa5b70500afb84824ab5660
ttaylor1248 <67172013+ttaylor1248@users.noreply.github.com>
2024-05-04 17:33:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,10 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be: ', name_on_order)
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME").col("SEARCH_ON"))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# pd_df = my_dataframe.to_pandas()
+
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -36,7 +35,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        
+        st.subheader(fruit_chosen + ' Nutrition Information')
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
